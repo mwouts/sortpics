9b1f4e83b3d3fe034a17a464e329147a1a6cb034
Marc Wouts <marc.wouts@gmail.com>
2020-05-16 19:05:20 +0200

    f-strings are available in Python >= 3.6

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Environment :: Console",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
