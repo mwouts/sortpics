5251702834b13b48b36275154b9afa1074f001cc
Marc Wouts <marc.wouts@gmail.com>
2020-05-17 00:14:09 +0200

    Alternative fromisoformat for Python 3.6

--- tests/test_icloud_photos.py
+++ tests/test_icloud_photos.py
@@ -1,11 +1,11 @@
 """Test sortpics on an ICloud photo collection extracted with icloudpd"""
-from datetime import datetime
 from sortpics.normalize import normalize_filename
+from sortpics.timestamp import fromisoformat
 
 
 def test_matching_name_date(
     path="2011/12/25/2011-12-15 14.49.56.jpg",
-    exif_date=datetime.fromisoformat("2011-12-15 14:49:56"),
+    exif_date=fromisoformat("2011-12-15 14:49:56.000000"),
     target_name="2011-12-15 14.49.56.jpg",
 ):
     assert normalize_filename(path, exif_date) == target_name
@@ -13,7 +13,7 @@ def test_matching_name_date(
 
 def test_exif_date_overrides_name(
     path="2011/12/25/2011-12-15 14.49.56.jpg",
-    exif_date=datetime.fromisoformat("2011-12-15 14:49:57"),
+    exif_date=fromisoformat("2011-12-15 14:49:57"),
     target_name="2011-12-15 14.49.57.jpg",
 ):
     assert normalize_filename(path, exif_date) == target_name
--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,6 +1,6 @@
-from datetime import datetime
 from sortpics.tags import parse_tags
 from sortpics.normalize import normalize_filename
+from sortpics.timestamp import fromisoformat
 
 
 def test_parse_tags():
@@ -13,7 +13,7 @@ def test_parse_tags():
 
 def test_keep_tag(
     path="2011/12/25/Cute cat.jpg",
-    exif_date=datetime.fromisoformat("2011-12-15 14:49:56"),
+    exif_date=fromisoformat("2011-12-15 14:49:56"),
     target_name="2011-12-15 14.49.56 Cute cat.jpg",
 ):
     assert normalize_filename(path, exif_date) == target_name
@@ -21,7 +21,7 @@ def test_keep_tag(
 
 def test_keep_tag_after_date(
     path="2011/12/25/2011-12-15 14.49.56 Cute cat.jpg",
-    exif_date=datetime.fromisoformat("2011-12-15 14:49:56"),
+    exif_date=fromisoformat("2011-12-15 14:49:56"),
     target_name="2011-12-15 14.49.56 Cute cat.jpg",
 ):
     assert normalize_filename(path, exif_date) == target_name
@@ -29,7 +29,7 @@ def test_keep_tag_after_date(
 
 def test_keep_tag_after_image_name(
     path="2011/12/25/IMG_2505 Cute cat.jpg",
-    exif_date=datetime.fromisoformat("2011-12-15 14:49:56"),
+    exif_date=fromisoformat("2011-12-15 14:49:56"),
     target_name="2011-12-15 14.49.56 Cute cat.jpg",
 ):
     assert normalize_filename(path, exif_date) == target_name
